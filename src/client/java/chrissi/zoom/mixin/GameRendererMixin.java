e687bfb0206a7c4724862d3410fd658d5a634e23
chrissi <chrissii.txt@gmail.com>
2026-01-12 20:17:13 +0100

    Update GameRendererMixin.java

--- src/client/java/chrissi/zoom/mixin/GameRendererMixin.java
+++ src/client/java/chrissi/zoom/mixin/GameRendererMixin.java
@@ -1,26 +1,18 @@
 package chrissi.zoom.mixin;
 
-import chrissi.zoom.ZoomHandler;
-import net.minecraft.client.Minecraft;
+import chrissi.zoom.ZoomClient;
 import net.minecraft.client.renderer.GameRenderer;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(GameRenderer.class)
 public class GameRendererMixin {
-    @Shadow @Final private Minecraft minecraft;
 
+    // modify fov to apply zoom effect
     @Inject(method = "getFov", at = @At("RETURN"), cancellable = true)
     private void applyZoom(CallbackInfoReturnable<Float> cir) {
-        if (this.minecraft.player != null) {
-            float zoomLevel = ZoomHandler.getZoomLevel();
-            if (zoomLevel < 1.0f) {
-                cir.setReturnValue(cir.getReturnValue() * zoomLevel);
-            }
-        }
+        cir.setReturnValue(ZoomClient.getInstance().modifyFov(cir.getReturnValue()));
     }
 }
